cfdf82b95df7cf39d34359eb16a7afe27e0e911e
June Shim <jys2@sfu.ca>
2019-07-26 21:05:14 -0700

    Cleanup import statements (JS)

--- predict_review_by_plots_step2a_rounding.py
+++ predict_review_by_plots_step2a_rounding.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
 import sys
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.pipeline import make_pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import SGDClassifier
